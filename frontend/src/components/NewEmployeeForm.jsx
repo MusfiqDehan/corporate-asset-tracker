774357dd86a4fb4c8af5e17ce43c18f228d25b43
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-11-06 07:02:00 +0600

    Convert class component to functional component

--- frontend/src/components/NewEmployeeForm.jsx
+++ frontend/src/components/NewEmployeeForm.jsx
@@ -1,95 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import axios from "axios";
 import { API_URL } from "../constants";
 
-class NewEmployeeForm extends React.Component {
-    state = {
+const NewEmployeeForm = (props) => {
+    const [employee, setEmployee] = useState({
         pk: 0,
         name: "",
         email: "",
         document: "",
         phone: "",
-    };
+    });
 
-    componentDidMount() {
-        if (this.props.employee) {
-            const { pk, name, document, email, phone } = this.props.employee;
-            this.setState({ pk, name, document, email, phone });
+    useEffect(() => {
+        if (props.employee) {
+            const { pk, name, document, email, phone } = props.employee;
+            setEmployee({ pk, name, document, email, phone });
         }
-    }
+    }, [props.employee]);
 
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
+    const onChange = (e) => {
+        setEmployee({ ...employee, [e.target.name]: e.target.value });
     };
 
-    createStudent = (e) => {
+    const createStudent = (e) => {
         e.preventDefault();
-        axios.post(API_URL, this.state).then(() => {
-            this.props.resetState();
-            this.props.toggle();
+        axios.post(API_URL, employee).then(() => {
+            props.resetState();
+            props.toggle();
         });
     };
 
-    editStudent = (e) => {
+    const editStudent = (e) => {
         e.preventDefault();
-        axios.put(API_URL + this.state.pk, this.state).then(() => {
-            this.props.resetState();
-            this.props.toggle();
+        axios.put(API_URL + employee.pk, employee).then(() => {
+            props.resetState();
+            props.toggle();
         });
     };
 
-    defaultIfEmpty = (value) => {
+    const defaultIfEmpty = (value) => {
         return value === "" ? "" : value;
     };
 
-    render() {
-        return (
-            <Form
-                onSubmit={
-                    this.props.employee ? this.editStudent : this.createStudent
-                }
-            >
-                <FormGroup>
-                    <Label for="name">Name:</Label>
-                    <Input
-                        type="text"
-                        name="name"
-                        onChange={this.onChange}
-                        value={this.defaultIfEmpty(this.state.name)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="email">Email:</Label>
-                    <Input
-                        type="email"
-                        name="email"
-                        onChange={this.onChange}
-                        value={this.defaultIfEmpty(this.state.email)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="document">Document:</Label>
-                    <Input
-                        type="text"
-                        name="document"
-                        onChange={this.onChange}
-                        value={this.defaultIfEmpty(this.state.document)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="phone">Phone:</Label>
-                    <Input
-                        type="text"
-                        name="phone"
-                        onChange={this.onChange}
-                        value={this.defaultIfEmpty(this.state.phone)}
-                    />
-                </FormGroup>
-                <Button>Send</Button>
-            </Form>
-        );
-    }
-}
+    return (
+        <Form onSubmit={props.employee ? editStudent : createStudent}>
+            <FormGroup>
+                <Label for="name">Name:</Label>
+                <Input
+                    type="text"
+                    name="name"
+                    onChange={onChange}
+                    value={defaultIfEmpty(employee.name)}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="email">Email:</Label>
+                <Input
+                    type="email"
+                    name="email"
+                    onChange={onChange}
+                    value={defaultIfEmpty(employee.email)}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="document">Document:</Label>
+                <Input
+                    type="text"
+                    name="document"
+                    onChange={onChange}
+                    value={defaultIfEmpty(employee.document)}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="phone">Phone:</Label>
+                <Input
+                    type="text"
+                    name="phone"
+                    onChange={onChange}
+                    value={defaultIfEmpty(employee.phone)}
+                />
+            </FormGroup>
+            <Button>Send</Button>
+        </Form>
+    );
+};
 
 export default NewEmployeeForm;
