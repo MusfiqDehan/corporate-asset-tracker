3d1b79f2a515a4da597ad7aa809ba4b88d556717
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-11-06 07:05:54 +0600

    Remove unnecessary imports

--- frontend/src/components/EmployeeList.jsx
+++ frontend/src/components/EmployeeList.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Table } from "reactstrap";
 import NewEmployeeModal from "./NewEmployeeModal";
 import ConfirmRemovalModal from "./ConfirmRemovalModal";
--- frontend/src/components/NewEmployeeForm.jsx
+++ frontend/src/components/NewEmployeeForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import axios from "axios";
 import { API_URL } from "../constants";
